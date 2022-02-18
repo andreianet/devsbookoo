886c20e74b0c5b827a4c8321b4830bc8e6c0b223
andreianet <andreia.netsys@hotmail.com>
2022-02-18 16:55:57 -0300

    updated file

--- models/Auth.php
+++ models/Auth.php
@@ -6,11 +6,13 @@ class Auth
 {
     private $pdo;
     private $base;
+    private $dao;
 
     public function __construct(PDO $pdo, $base)
     {
         $this->pdo = $pdo;
         $this->base = $base;
+        $this->dao = new UserDaoMysql($this->pdo);
     }
     /**
      * Verificar token 
@@ -19,10 +21,8 @@ class Auth
     {
         if (!empty($_SESSION['token'])) {
            $token = $_SESSION['token'];
-
-           $userDAO = new UserDaoMysql($this->pdo);
-           $user = $userDAO->findByToken($token);
-
+           //$userDAO = new UserDaoMysql($this->pdo);
+           $user = $this->dao->findByToken($token);
            if ($user) {
               return $user;
            }
@@ -31,6 +31,55 @@ class Auth
         header("Location ".$this->base."/login.php");
         exit;
     }
+
+    public function validateLogin($email, $password)
+    {
+        //$userDAO = new UserDaoMysql($this->pdo);
+        /**
+         * Varificar Login
+         */
+        $user = $this->dao->findByEmail($email);
+        if ($user) {
+            
+            if (password_verify($password, $user->password)) {
+                $token = md5(time().rand(0, 9999));
+
+                $_SESSION['token'] = $token;
+                $user->token = $findByToken($token);
+                $this->dao->update($user);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+    /**
+     * E-mail if exists
+     */
+    public function emailExists($email)
+    {
+        //$userDAO = new UserDaoMysql($this->pdo);
+        return $this->dao->findByEmail($email) ? true : false;
+    }
+
+    public function registerUser($name,$email, $password, $birthdate)
+    {
+        $userDAO = new UserDaoMysql($this->pdo);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $token = md5(time().rand(0, 9999));
+
+        $newUser = new User();
+        $newUser->name = $name;
+        $newUser->email = $email;
+        $newUser->password = $password;
+        $newUser->birthdate = $birthdate;
+
+        $this->dao->insert($newUser);
+
+        $_SESSION['token'] = $token;
+    }
+
 
 }
 ?>
